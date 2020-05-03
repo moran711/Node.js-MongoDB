47ea11724b65a08d3cd64e4834ab577e4219c5df
moran711 <59802190+moran711@users.noreply.github.com>
2020-05-03 16:33:25 +0300

    Add files via upload

---
+++ routes/add.js
@@ -0,0 +1,28 @@
+const {Router} = require('express');
+const Course = require('../models/course');
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.render('add', {
+    title: 'Добавить курс',
+    isAdd: true
+  });
+});
+
+router.post('/', async (req, res) => {
+  // const course = new Course(req.body.title, req.body.price, req.body.img)
+  const course = new Course({
+    title: req.body.title, 
+    price: req.body.price, 
+    img: req.body.img
+  })
+  try { 
+    await course.save();
+  } catch(e) {
+    console.log(e);
+  }
+
+  res.redirect('/courses');
+});
+
+module.exports = router;
